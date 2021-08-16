6bd599325f70d1f6f825a08312f4c60a22ea2ab2
Seetharaman <Seetharaman.Venkatesan@target.com>
2021-08-15 21:21:24 -0500

    Updating mapping + Don't send nulls

--- src/main/kotlin/com/myretail/products/prices/controllers/PricesController.kt
+++ src/main/kotlin/com/myretail/products/prices/controllers/PricesController.kt
@@ -11,14 +11,14 @@ import org.springframework.web.bind.annotation.RestController
 import java.math.BigDecimal
 
 @RestController
-@RequestMapping(path = ["/v1"], produces = [MediaType.APPLICATION_JSON_VALUE])
+@RequestMapping(path = ["/v1/prices"], produces = [MediaType.APPLICATION_JSON_VALUE])
 class PricesController {
 
-    @GetMapping("/products/{productId}/prices")
+    @GetMapping("/products/{productId}")
     fun getPricesByProductId(
         @PathVariable productId: Long
 //        ,@RequestParam("includes", defaultValue = "[]") includes: List<PriceType>
     ): PricesDocument {
-        return PricesDocument(productId, AllPrices(Price(BigDecimal.ONE.toDouble(), "USD"), null, null))
+        return PricesDocument(productId, AllPrices(Price(BigDecimal.ONE.toDouble(), "USD")))
     }
 }

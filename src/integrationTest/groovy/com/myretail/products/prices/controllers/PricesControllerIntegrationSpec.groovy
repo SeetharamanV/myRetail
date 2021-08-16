86df3def925553eca3b09d4cfb2a7cf6a77c57a1
Seetharaman <Seetharaman.Venkatesan@target.com>
2021-08-15 21:17:57 -0500

    Adding a setup to ignore null values

--- src/integrationTest/groovy/com/myretail/products/prices/controllers/PricesControllerIntegrationSpec.groovy
+++ src/integrationTest/groovy/com/myretail/products/prices/controllers/PricesControllerIntegrationSpec.groovy
@@ -7,10 +7,10 @@ class PricesControllerIntegrationSpec extends AbstractRestIntegrationSpecificati
 
     def "Test Price Core Controller - Get Happy "() {
         given:
-        String expectedResponse = """{"product_id":1233,"prices":{"current_price":{"value":1.0,"currency_code":"USD"},"regular_price":null,"initial_price":null}}""" as String
+        String expectedResponse = """{"product_id":1233,"prices":{"current_price":{"value":1.0,"currency_code":"USD"}}}""" as String
 
         when: "Get prices for an item, happy path."
-        MvcResult result = mockGet("/v1/products/1233/prices").andReturn()
+        MvcResult result = mockGet("/v1/prices/products/1233").andReturn()
 
         then:
         result.response.status == 200
@@ -19,7 +19,7 @@ class PricesControllerIntegrationSpec extends AbstractRestIntegrationSpecificati
 
     def "Test Price Core Controller - Bad Request "() {
         when: "Get an item, bad request."
-        MvcResult result = mockGet("/v1/products/ABC/prices").andReturn()
+        MvcResult result = mockGet("/v1/prices/products/ABC").andReturn()
 
         then:
         result.response.status == 400
--- src/main/kotlin/com/myretail/products/prices/entities/PricesDocument.kt
+++ src/main/kotlin/com/myretail/products/prices/entities/PricesDocument.kt
@@ -1,14 +1,17 @@
 package com.myretail.products.prices.entities
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
 data class PricesDocument(
     val productId: Long,
     val prices: AllPrices
 )
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class AllPrices(
-    val currentPrice: Price?,
-    val regularPrice: Price?,
-    val initialPrice: Price?,
+    val currentPrice: Price? = null,
+    val regularPrice: Price? = null,
+    val initialPrice: Price? = null,
 )
 
 data class Price(

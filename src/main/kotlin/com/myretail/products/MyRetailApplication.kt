d883bd74a148bc9c409048e7d58c6f987c934c9c
Seetharaman <Seetharaman.Venkatesan@target.com>
2021-08-23 00:29:43 -0500

    Adding Mongock
    1. Document structure validation
    2. Creating index on productIdd

--- src/main/kotlin/com/myretail/products/MyRetailApplication.kt
+++ src/main/kotlin/com/myretail/products/MyRetailApplication.kt
@@ -1,11 +1,13 @@
 package com.myretail.products
 
+import com.github.cloudyrock.spring.v5.EnableMongock
 import com.myretail.products.configs.PartnerApisProperties
 import com.myretail.products.security.SecurityProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
+@EnableMongock
 @SpringBootApplication
 @EnableConfigurationProperties(SecurityProperties::class, PartnerApisProperties::class)
 class MyRetailApplication
